dfc81b26bdb667932753d78b2e4384025e48bd0f
GHGHGHKO <gudrb963@gmail.com>
2022-11-15 23:08:07 +0900

    feat: test instance lifecycle

--- src/test/kotlin/me/synology/kotlinblog/controller/HelloControllerTest.kt
+++ src/test/kotlin/me/synology/kotlinblog/controller/HelloControllerTest.kt
@@ -1,6 +1,9 @@
 package me.synology.kotlinblog.controller
 
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.TestConstructor
@@ -10,13 +13,19 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 @Transactional
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 internal class HelloControllerTest(
         val mockMvc: MockMvc
 ) {
+
+    @BeforeAll
+    fun setup() {
+        println(">> Setup")
+    }
 
     @Test
     fun `hello Kotlin`() {
@@ -24,5 +33,10 @@ internal class HelloControllerTest(
         mockMvc.perform(get("/hello/{language}", "kotlin"))
                 .andExpect(status().isOk)
                 .andExpect(MockMvcResultMatchers.content().string("kotlin"))
+    }
+
+    @AfterAll
+    fun teardown() {
+        println(">> Tear down")
     }
 }
