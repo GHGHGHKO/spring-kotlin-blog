db92894059798f5c2c5a38b53851e01297d60711
GHGHGHKO <gudrb963@gmail.com>
2022-11-15 22:11:16 +0900

    Initial commit

---
+++ src/main/kotlin/me/synology/kotlinblog/KotlinBlogApplication.kt
@@ -0,0 +1,11 @@
+package me.synology.kotlinblog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinBlogApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinBlogApplication>(*args)
+}
