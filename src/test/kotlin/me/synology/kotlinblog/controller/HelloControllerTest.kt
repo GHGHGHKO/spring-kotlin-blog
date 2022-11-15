613424901a3fae2bb263fa424829181270f09f38
GHGHGHKO <gudrb963@gmail.com>
2022-11-15 22:42:32 +0900

    feat: hello kotlin test code

---
+++ src/test/kotlin/me/synology/kotlinblog/controller/HelloControllerTest.kt
@@ -0,0 +1,28 @@
+package me.synology.kotlinblog.controller
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestConstructor
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+internal class HelloControllerTest(
+        val mockMvc: MockMvc
+) {
+
+    @Test
+    fun helloKotlin() {
+
+        mockMvc.perform(get("/hello/{language}", "kotlin"))
+                .andExpect(status().isOk)
+                .andExpect(MockMvcResultMatchers.content().string("kotlin"))
+    }
+}
